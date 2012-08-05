fb81fb45e259a58f4df0c130b2457937bec1d85e
Jonathan Vasquez <jvasquez1011@gmail.com>
2012-08-05 04:24:22 -0400

    installer is no more, now it's special scripts

---
+++ resources/variables.sh
@@ -0,0 +1,20 @@
+# Copyright (C) 2012 Jonathan Vasquez <jvasquez1011@gmail.com>
+#
+# Distributed under the ISC license which can be found in the LICENSE file.
+
+# Application Info
+JV_APP_NAME="Bliss Toolkit"
+JV_AUTHOR="Jonathan Vasquez"
+JV_EMAIL="jvasquez1011@gmail.com"
+JV_CONTACT="${JV_AUTHOR} <${JV_EMAIL}>"
+JV_VERSION="1.0.3"
+JV_LICENSE="ISC"
+
+# Locations
+PREFIX="/mnt/gentoo"
+
+# Downloaded files will be saved here
+FILESDIR="files"
+
+# Default directories to create with 'beforemath'
+DEFAULT_DIRS="boot home var usr/portage usr/src"
